9ef5f2f96084a42c66a27f8d021179f96e46b566
PaPPu <pappu170101019@outlook.com>
2023-02-10 19:35:12 +0600

    Even or Odd checking

---
+++ Even Odd Number Calculation/Even Odd Number Calculation/Program.cs
@@ -0,0 +1,28 @@
+﻿internal class Program
+{
+    public class EvenOddCalculation
+    {
+        public void EvenOddCalculationMethod(int number)
+        {
+            if (number%2 == 0) 
+            {
+                Console.WriteLine(number + " is Even number.");
+            }
+            else if(number%2 == 1)
+            {
+                Console.WriteLine(number + " is Odd number.");
+            }
+            else
+            {
+                Console.WriteLine(number + "is wrong!!");
+            }
+        }
+    }
+    public static void Main(string[] args)
+    {
+        Console.Write("Enter number for check Even or Odd: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+        EvenOddCalculation method = new EvenOddCalculation();
+        method.EvenOddCalculationMethod(n);
+    }
+}
