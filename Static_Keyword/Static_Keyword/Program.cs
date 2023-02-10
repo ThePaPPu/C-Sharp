26db2078bfa41d62ef78d8b903ea752348011ea9
356219 <356219@LAP-0116-4405>
2023-02-10 14:36:10 +0600

    Static Method used from different class

---
+++ Static_Keyword/Static_Keyword/Program.cs
@@ -0,0 +1,13 @@
+﻿using Static_Keyword.Static_Class;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        int sum = Claculator.Sum(4, 4);
+        Console.WriteLine(sum);
+
+        int mul = Claculator.Mul(4, 4);
+        Console.WriteLine(mul);
+    }
+}
---
+++ Static_Keyword/Static_Keyword/Static_Class/Claculator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static_Keyword.Static_Class
+{
+    public static class Claculator
+    {
+        public static int Sum(int n1, int n2)
+        {
+            return n1 + n2;
+        }
+
+        public static int Mul(int n1, int n2)
+        {
+            return n1 * n2;
+        }
+    }
+}
